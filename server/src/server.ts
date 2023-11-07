8586244061b810663c56530c34ee6b1049ca89ee
p0ger <aharonibar1@gmail.com>
2023-11-07 20:03:56 +0200

    Added environmental variables and mongoDB connection

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,22 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+const mongodbUri = process.env.MONGODB_URI || '';
 
 app.get('/', (req, res) => {
-  res.send('Noder neder secured!');
+  res.send('Noder neder secured server!');
 });
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
+});
+
+mongoose.connect(mongodbUri).then(() => {
+  console.log('Successfully connected to mongoDB');
+}).catch(() => {
+  console.log('Failed to connect to mongoDB');
 });
