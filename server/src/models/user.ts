bece1b8da62c3f36705ce0e823edd16f005f1aff
p0ger <aharonibar1@gmail.com>
2023-11-07 20:02:50 +0200

    Created user model

---
+++ server/src/models/user.ts
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            minlength: 3,
+            maxlength:30
+            },
+        email: {
+            type: String,
+            required: true,
+            minlength: 3,
+            maxlength: 200,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 3,
+            maxlength:1024
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+const userModel = mongoose.model('User', UserSchema);
+
+export default userModel;
