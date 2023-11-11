4e74dfd442566ede8b75512697e87c275e33ed82
dvir2006 <61055320+dvir2006@users.noreply.github.com>
2023-11-11 20:44:45 +0200

    Added function for protected endpoints

--- client/src/services/Server.tsx
+++ client/src/services/Server.tsx
@@ -1,17 +1,27 @@
-export const apiUrl = "http://localhost:5000/api/"
+export const apiUrl = "http://localhost:5000/api"
 
 export const PostRequest = async (url: string, body:any) :Promise<Response> => {
-    const response = await fetch(url, {
+    console.log(url, body);
+    const res = await fetch(url, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify(body),
     })
-    return response;
+    return res;
 }
 
 export const GetRequest = async (url: string) :Promise<Response> => {
     const response = await fetch(url);
+    return response;
+}
+
+export const ProtectedGetRequest = async (url: string,jwtToken:string) :Promise<Response> => {
+    const response = await fetch(url,{
+        headers: {
+            Authorization: jwtToken,
+        }
+    });
     return response;
 }
--- client/src/utils/signals.ts
+++ client/src/utils/signals.ts
@@ -1,4 +1,5 @@
-import { signal } from "@preact/signals";
+import { signal } from "@preact/signals-react";
+import { createContext } from "preact";
 
 
 export const username = signal("");
@@ -6,3 +7,5 @@ export const email = signal("");
 export const password = signal("");
 export const auth = signal({isLoggedIn: false});
 export const error = signal("");
+
+export const isLoggedIn = createContext(false);
