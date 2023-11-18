88376f2d6f71ea78f630cfbea863bd8891089915
dvir2006 <61055320+dvir2006@users.noreply.github.com>
2023-11-18 18:08:04 +0200

    added chat props

--- client/src/utils/types.ts
+++ client/src/utils/types.ts
@@ -8,3 +8,19 @@ export interface MainPageProps{
     user: any;
 }
 
+export interface ChatAppProps{
+    open: boolean,
+    onClose: () => void,
+    users: any,
+    onChat: any
+}
+
+export interface ChatWindowProps{
+    chat: any,
+    currUser: any
+}
+export interface MessageType{
+    sender: string,
+    content: string,
+    timestamp: Date
+}
