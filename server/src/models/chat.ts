5ce6d357181e876f43916f8e30eba8bd5908cb12
dvir2006 <61055320+dvir2006@users.noreply.github.com>
2023-11-18 16:05:29 +0200

    Created chat schema

---
+++ server/src/models/chat.ts
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+
+const ChatSchema = new mongoose.Schema(
+    {
+        type: {
+            type: String,
+            enum: ["private", "group"],
+            required: true,
+            minlength: 5,
+            maxlength:8    
+        },
+        name: {
+            type: String,
+            required: false,
+            minlength: 3,
+            maxlength:30  
+        },
+        users: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }],
+        messages: [
+            {
+              sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+              content: String,
+              timestamp: { type: Date, default: Date.now }
+            }
+        ],
+    },
+    {
+        timestamps: true
+    }
+);
+const chatModel = mongoose.model('Chat', ChatSchema);
+
+export default chatModel;
