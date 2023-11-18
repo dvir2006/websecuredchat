a30886dc78ecc3e1114ba565e3e4ab4a93c804b6
dvir2006 <61055320+dvir2006@users.noreply.github.com>
2023-11-18 18:09:34 +0200

    Added protected post request helper function

--- client/src/services/Server.tsx
+++ client/src/services/Server.tsx
@@ -1,7 +1,6 @@
 export const apiUrl = "http://localhost:5000/api"
 
 export const PostRequest = async (url: string, body:any) :Promise<Response> => {
-    console.log(url, body);
     const res = await fetch(url, {
         method: 'POST',
         headers: {
@@ -22,6 +21,17 @@ export const ProtectedGetRequest = async (url: string,jwtToken:string) :Promise<
         headers: {
             Authorization: jwtToken,
         }
+    });
+    return response;
+}
+export const ProtectedPostRequest = async (url: string,body:any, jwtToken:string) :Promise<Response> => {
+    const response = await fetch(url,{
+        method: 'POST',
+        headers: {
+            Authorization: jwtToken,
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
     });
     return response;
 }
