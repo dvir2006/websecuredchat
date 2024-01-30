f2c58e2dd97bc5a1b982a1836a2ba4c072ad4bb5
dvir2006 <61055320+dvir2006@users.noreply.github.com>
2024-01-30 15:22:31 +0200

    redesigned header and footer

--- client/src/components/const/Footer.tsx
+++ client/src/components/const/Footer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Link } from "@mui/material";
 
 const Footer: React.FC = () => {
   return (
@@ -9,14 +9,21 @@ const Footer: React.FC = () => {
           width: "100%",
           position: "sticky",
           bottom: 0,
-          bgcolor: "#fff",
-          color: "#000",
-          padding: "10px 0",
+          bgcolor: "#f8f9fa", 
+          color: "#343a40", 
+          padding: "20px 0", 
           textAlign: "center",
+          borderTop: "1px solid #ced4da", 
           zIndex: 1000,
         }}
       >
-        <Typography variant="h6">Sigma</Typography>
+        <Typography variant="h6" gutterBottom>
+          Sigma
+        </Typography>
+        <Typography variant="body2" color="textSecondary">
+          © {new Date().getFullYear()} All rights reserved.
+        </Typography>
+
       </Box>
     </footer>
   );
--- client/src/components/const/Header.tsx
+++ client/src/components/const/Header.tsx
@@ -9,11 +9,13 @@ const Header: React.FC = () => {
           width: "100%",
           position: "sticky",
           top: 0,
-          bgcolor: "#fff",
-          color: "#000",
+          bgcolor: "#f8f9fa",
+          color: "#343a40",
           padding: "10px 0",
           textAlign: "center",
           zIndex: 1000,
+          marginBottom: ".5%",
+          borderBottom: "1px solid #ced4da",
         }}
       >
         <Typography variant="h4">Seqchat</Typography>
